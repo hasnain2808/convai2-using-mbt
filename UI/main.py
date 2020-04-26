a9269a4535302e0145b87f7aff27557997c62bb0
hasnain2808 <hasnain2808@gmail.com>
2020-04-26 08:58:05 +0530

    stt and tts

--- UI/main.py
+++ UI/main.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    requests.get('http://127.0.0.1:5000/gen_personality' )
+    # requests.get('http://127.0.0.1:5000/gen_personality' )
 
     return render_template('widgets.html')
 
@@ -14,11 +14,15 @@ def home():
 
 @app.route('/new_message', methods=['GET', 'POST'])
 def new_message():
+    # if the input is present Nothing received is used
     raw_text = "Nothing received"
+    # from the user
     if 'input_dialogue' in request.args:
         raw_text = str(request.args['input_dialogue'])
 
-    x = requests.get('http://127.0.0.1:5000/', params={ "input_dialogue" : raw_text} )
+    # x = requests.get('http://127.0.0.1:5000/', params={ "input_dialogue" : raw_text} )
 
-    return x.content
+    # return x.content
+    # //to the user
+    return raw_text
 app.run(port=5001)
